55b643659d9e7cd27bf910d616eeaf88d6576979
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-13 20:48:04 +0000

    restructure files

---
+++ server/app.js
@@ -0,0 +1,11 @@
+// routes - when a get request is made
+app.get('/api', function(req, res) {
+    // redirect client to game room (using dynamic routing)
+
+    //req.query.q
+
+    //result = run3Apis(q)
+
+    //res.send(results)
+
+});
