f92b1f84df1f74fb518e3737ead2384bde22b9a2
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-14 03:14:18 +0000

    housekeeping

--- server/app.js
+++ server/app.js
@@ -1,3 +1,6 @@
+
+// pseudo code for the server that should hit 3 APIs
+// and get aggregate results
 // routes - when a get request is made
 app.get('/api', function(req, res) {
     // redirect client to game room (using dynamic routing)
