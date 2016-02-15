3fc6424744a69c9cfb19f608ce75853c93ab2bf7
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-15 22:29:45 +0000

    got titles of the top ranking articles

--- server/app.js
+++ server/app.js
@@ -3,13 +3,10 @@
 let request = require("request");
 let cheerio = require("cheerio");
 
-request('https://news.ycombinator.com/', (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-        // parse returned HTML
-        let $ = cheerio('html');
-        console.log($);
-        // go to each span with class sitebit and grab element above it
-        $('span.sitebit').each(function(i, element) {
+request('https://news.ycombinator.com', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+        let $ = cheerio.load(html);
+        $('span.comhead').each(function(i, element){
             let a = $(this).prev();
             console.log(a.text());
         });
