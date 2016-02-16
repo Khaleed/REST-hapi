8b3c9c3876401abc483cb4f94573940a0918a031
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-16 04:06:37 +0000

    finished scraping

--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
 "use strict";
 
-let request = require("request");
-let cheerio = require("cheerio");
+let request = require('request');
+let cheerio = require('cheerio');
+let Immutable = require('immutable');
 
 request('https://news.ycombinator.com', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         let $ = cheerio.load(html);
-        $('span.comhead').each(function(i, element){
+        let parsedResults = Immutable.List([]);
+        let newParsedResults;
+        $('span.comhead').each(function(i, element) {
             let a = $(this).prev();
-            console.log(a.text());
+            let rank = a.parent().parent().text();
+            let title = a.text();
+            let url = a.attr('href');
+            let subtext = a.parent().parent().next().children('.subtext').children();
+            let credits = $('.subtext').eq(0).text();
+            let metadata = {
+                rank: parseInt(rank),
+                title: title,
+                url: url,
+                credits: credits
+            };
+            newParsedResults = parsedResults.push(metadata);
         });
+        console.log(newParsedResults);
     }
 });
