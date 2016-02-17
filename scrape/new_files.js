ea7c94539591133c2fb7b9f66539d541e6b08286
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-17 03:03:40 +0000

    add express

--- scrape/new_files.js
+++ scrape/new_files.js
@@ -3,30 +3,32 @@
 let request = require('request');
 let cheerio = require('cheerio');
 let express = require('express');
-let app = express();
 
-app.get('/hn_front_page', req, res => {
-    getResponse((err, result) => {
+//some kind of express setup
+
+var app = null;
+
+app.get('/scrape_hn', function (req, resp) {
+    makeResponse(function (err, result) {
         if (err) {
-            res.status(400).send('Something is broken');
+            resp.sendFail(err);
         } else {
-            res.send(JSON.stringify(result));
+            resp.send(JSON.stringify(result));
         }
-    });
-});
 
-// get rank, title, url details for HN articles
-let getResponse = callback => {
+    });
+ });
+
+function makeResponse (callback) {
+    // get rank, title, url details for HN articles
     request('https://news.ycombinator.com', function (error, response, html) {
         if (error) {
-            console.log('error');
-            callback(error);
+            callback(error, null);
         } else if (response.statuscode !== 200) {
-            console.log('try again');
-            callback(new Error('error try again'));
+            callback('not ok');
         } else {
             let $ = cheerio.load(html);
-            let newparsedresults = $('span.comhead').map(function (i, element) {
+            let newparsedresults = $('span.comhead').map(function(i, element) {
                 let a = $(this).prev();
                 let rank = a.parent().parent().text();
                 let title = a.text();
@@ -44,7 +46,3 @@ let getResponse = callback => {
         }
     });
 }
-
-app.listen(3000, () => {
-    console.log('Listen to port 3000');
-});
