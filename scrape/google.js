256e42e64eb19cd9bb3686318ca90ce56d5ef903
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-18 03:29:16 +0000

    wrote initial API and scraped google

--- scrape/google.js
+++ scrape/google.js
@@ -1,1 +1,24 @@
+"use strict";
 
+// google
+let google = require("google");
+
+google.resultsPerPage = 25;
+let nextCounter = 0;
+
+google('bengal tigers', function (err, next, links) {
+    if (err) {
+        console.error(err);
+    } else {
+        links.map(link => {
+            console.log(link.title + ' - ' + link.link) // link.href is an alias for link.link
+            console.log(links.description + "\n");
+        });
+        if (nextCounter < 4) {
+            nextCounter += 1
+            if (next) {
+                next();
+            }
+        }
+    }
+});
--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 
 "use strict";
 
-let google = require('google')
-let request = require('request');
 let cheerio = require('cheerio');
 let express = require('express');
 let app = express();
 
+// RESTful API that gets aggregated results
 app.get('/aggregate_search', (req, res) => {
     // 3 async requests
 
@@ -19,7 +18,7 @@ app.get('/aggregate_search', (req, res) => {
         x[0] = true;
         //do something with google response
         if (x.every(x => x == true)) {
-            res.send()
+            res.send();
         }
     });
     search_duck(search_query, (err, result) => {
@@ -34,18 +33,16 @@ app.get('/aggregate_search', (req, res) => {
 
     //when can you res.send()?
 
-
     /*getResponse((err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(400).send('Something is broken');
-        } else {
-            res.end(JSON.stringify(result));
-        }
-    });*/
+      if (err) {
+      console.log(err);
+      res.status(400).send('Something is broken');
+      } else {
+      res.end(JSON.stringify(result));
+      }
+      });*/
 });
 
-// get rank, title, url details for HN articles
 let getResponse = callback => {
     var req_callback = function (error, response, html) {
         if (error) {
@@ -55,48 +52,8 @@ let getResponse = callback => {
             console.log(response.statusCode);
             callback(new Error('error try again'));
         } else {
-            let $ = cheerio.load(html);
-            let newparsedresults = $('span.comhead').map(function (i, element) {
-                let a = $(this).prev();
-                let rank = a.parent().parent().text();
-                let title = a.text();
-                let url = a.attr('href');
-                let subtext = a.parent().parent().next().children('.subtext').children();
-                let credits = $('.subtext').eq(0).text();
-                let metadata = {
-                    rank: +rank,
-                    title: title,
-                    url: url,
-                    credits: credits
-                };
-                callback(null, metadata);
-            });
-        }
-    };
-    var x = 0;
-    request('https://news.ycombinator.com', callback);
-    x += 1;
-}
-
-app.listen(3000, () => {
-    console.log('Listen to port 3000');
-});
-
-// google
-
-google.resultsPerPage = 25;
-let nextCounter = 0;
-
-google('node.js best practices', function (err, next, links) {
-    if (err) console.error(err)
-
-    for (var i = 0; i < links.length; ++i) {
-        console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link
-        console.log(links[i].description + "\n")
-    }
-
-    if (nextCounter < 4) {
-        nextCounter += 1
-        if (next) next()
-    }
-})
+            // parse data
+        };
+        callback(null, metadata);
+    });
+};
