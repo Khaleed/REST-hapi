0e59c17014b2d48e3ae3f0b238d8df62997a0cad
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-19 04:42:53 +0000

    refactor and modularise

--- scrape/google.js
+++ scrape/google.js
@@ -2,21 +2,35 @@
 
 let google = require("google");
 google.resultsPerPage = 25;
-let nextCounter = 0;
 
-google('bengal tigers', (err, next, links) => {
-    if (err) {
-        console.error(err);
-    } else {
-        links.map(link => {
-            console.log(link.title + ' - ' + link.link);
-            console.log(link.description + "\n");
-        });
-        if (nextCounter < 4) {
-            nextCounter += 1
-            if (next) {
-                next();
+export function search (query, callback) {
+    let nextCounter = 0;
+    let results = [];
+    google(query, (err, next, links) => {
+        if (err) {
+            callback(err);
+        } else {
+            // user wants this
+            Array.prototype.push.apply(
+                results,
+                links.map(link =>
+                          {
+                              title: link.title,
+                              link: link.link,
+                              description: link.description
+                          };
+                         );
+            );
+            if (nextCounter < 4) {
+                nextCounter += 1
+                if (next) {
+                    next();
+                } else {
+                    callback(null, results);
+                }
+            } else {
+                callback(null, results);
             }
         }
-    }
-});
+    });
+}
