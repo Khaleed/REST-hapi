56aca26e748f6ade6620c39f15b6060eef5bf8d7
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-20 21:22:35 +0000

    re-define and run a new experiment

--- scrape/yahoo.js
+++ scrape/yahoo.js
@@ -3,20 +3,35 @@
 let request = require("request");
 let cheerio = require("cheerio");
 
-// send a request and query to yahoo
-request("https://yahoo.com", (error, response, html) => {
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode !== 200) {
-        console.log(response.statusCode);
-    } else {
-        // get HTML back and parse with Cheerio
-        let $ = cheerio.load(html);
-        console.log($);
-    }
-});
-
-
-
+// // send a request and query to yahoo
+// request("https://yahoo.com", (error, response, html) => {
+//     if (error) {
+//         console.log(error);
+//     } else if (response.statusCode !== 200) {
+//         console.log(response.statusCode);
+//     } else {
+//         // get HTML back and parse with Cheerio
+//         let $ = cheerio.load(html);
+//         console.log($);
+//     }
+// });
 
 // add callback that return data if there is no error
+
+// use nightmare
+
+let Nightmare = require("nightmare");
+let nightmare = require({show: true});
+
+nightmare
+    .goto('http://yahoo.com')
+    .type('input[title="Search"]', 'github nightmare')
+    .click('#uh-search-button')
+    .wait('#main')
+    .evaluate(function () {
+        return document.querySelector('#main .searchCenterMiddle li a').href
+    })
+    .end()
+    .then(function (result) {
+        console.log(result)
+    });
