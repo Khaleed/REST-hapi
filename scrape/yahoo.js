7c7e161a69df496b51ad993a05751b5609f22653
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-24 03:32:10 +0000

    back to cheerio

--- scrape/yahoo.js
+++ scrape/yahoo.js
@@ -1,35 +1,46 @@
 "use strict";
-// require modules
 let request = require("request");
 let cheerio = require("cheerio");
+let urls = [];
+// send a request and query to yahoo
+request("https://yahoo.com", (error, response, html) => {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode !== 200) {
+        console.log(response.statusCode);
+    } else {
+        let $ = cheerio.load(html);
+        let $searchResults = $(".reg .searchCenterMiddle");
+        console.log($searchResults.text());
+        let $links = $searchResults.children(".td-u");
+//        console.log($links);
+        let $link = $links.map((i, e) => {
+            console.log(e.href);c
+        });
+        let $title = $links.map((i, e) => {
+            console.log(e.text().replace(/\s+/g, " "));
+        });
+        let metadata = {
+            link: $link,
+            titlte : $title
+        }
+    }
+});
 
-// // send a request and query to yahoo
-// request("https://yahoo.com", (error, response, html) => {
-//     if (error) {
-//         console.log(error);
-//     } else if (response.statusCode !== 200) {
-//         console.log(response.statusCode);
-//     } else {
-//         // get HTML back and parse with Cheerio
-//         let $ = cheerio.load(html);
-//         console.log($);
-//     }
-// });
 
 // add callback that return data if there is no error
 
-// use nightmare
-let Nightmare = require("nightmare");
-let nightmare = Nightmare({ show: true });
-
-nightmare
-    .goto("https://yahoo.com")
-    .type("input[title='Search']", 'cats') // mimics a user typing in a text box
-    .click("#UHSearchWeb")
-    .wait("#main")
-    .evaluate(function () {
-        return document.querySelectorAll('#main .searchCenterMiddle li a').href;
-    })
-    .then(function (result) {
-        console.log(result);
-    });
+// let Nightmare = require("nightmare");
+// let nightmare = Nightmare({ show: true });
+// let parsedResults = [];
+// nightmare
+//     .goto("https://yahoo.com")
+//     .type("input[title='Search']", 'cats') // mimics a user typing in a text box
+//     .click("#UHSearchWeb")
+//     .wait("#main")
+//     .evaluate(function () {
+//         return document.querySelector('#main .searchCenterMiddle li a').href;
+//     })
+//     .then(function (result) {
+//         console.log(result);
+//     });
