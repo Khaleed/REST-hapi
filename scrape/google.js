ca43f7bec99f544156bc9f07ff64963be32e1fa6
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-11 16:26:38 +0100

    modularise and refactor

--- scrape/google.js
+++ scrape/google.js
@@ -1,36 +1,43 @@
 "use strict";
 
 let google = require('google');
+let Lists = require('./lists');
 google.resultsPerPage = 25;
 
-export function searchGoogle (query, callback) {
-    let nextCounter = 0;
-    let results = [];
-    google(query, (err, next, links) => {
-        if (err) {
-            callback(err);
-        } else {
-            // user wants this
-            Array.prototype.push.apply(
-                results,
-                links.map(link =>
-                          {
-                              title: link.title,
-                              link: link.link,
-                              description: link.description
-                          };
-                         );
-            );
-            if (nextCounter < 4) {
-                nextCounter += 1
-                if (next) {
-                    next();
+/**
+ * Searches google given some query
+ * @param {String} query - search query
+ * @param {Function} callback -std node callback
+ */
+module.exports = {
+    searchGoogle: function (query, callback) {
+        let nextCounter = 0;
+        let results = [];
+        google(query, (err, next, links) => {
+            if (err) {
+                console.log("hi");
+                callback(err);
+            } else {
+                console.log("hi callback");
+                let newLinks = links.map(link => {
+                    return {
+                        title: link.title,
+                        link: link.link,
+                        description: link.description
+                    };
+                });
+                Lists.appendAll(results, newLinks);
+                if (nextCounter < 4) {
+                    nextCounter += 1;
+                    if (next) {
+                        next();
+                    } else {
+                        callback(null, results);
+                    }
                 } else {
                     callback(null, results);
                 }
-            } else {
-                callback(null, results);
             }
-        }
-    });
-}
+        });
+    }
+};
