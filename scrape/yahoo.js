2851bd4577b1f7ec0f2a2e669ea607f3351948c9
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-11 19:27:42 +0100

    continue scraping yahoo

--- scrape/yahoo.js
+++ scrape/yahoo.js
@@ -4,28 +4,27 @@ let request = require("request");
 let cheerio = require("cheerio");
 let urls = [];
 // send a request and query to yahoo
-request("https://yahoo.com", (error, response, html) => {
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode !== 200) {
-        console.log(response.statusCode);
-    } else {
-        let $ = cheerio.load(html);
-        let $searchResults = $(".reg .searchCenterMiddle");
-        console.log($searchResults.text());
-        let $links = $searchResults.children(".td-u");
-        //console.log($links);
-        let $link = $links.map((i, e) => {
-            console.log(e.href);c
+
+module.exports = {
+    searchYahoo: function(searchQuery) {
+        return request("https://yahoo.com/search?p=" + searchQuery, (error, response, html) => {
+            if (error) {
+                console.log(error);
+            } else if (response.statusCode !== 200) {
+                console.log(response.statusCode);
+            } else {
+                let $ = cheerio.load(html);
+                console.log($);
+                let $searchResults = $(".reg.searchCenterMiddle").find(".dd.algo");
+                // return an array of search results
+                return $searchResults.map((i, el) => {
+                    return {
+                        title: $(el).find(".title"),
+                        link: $(el).find(".title .td-u").attr("href"),
+                        description: $(el).find(".comText aAbs .p").text()
+                    };
+                });
+            }
         });
-        let $title = $links.map((i, e) => {
-            console.log(e.text().replace(/\s+/g, " "));
-        });
-        let metadata = {
-            link: $link,
-            title : $title
-        };
     }
-});
-
-
+};
