68885bbef7a0904da83652256512cb196c93dbe2
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-18 00:16:45 +0100

    start scraping bing

--- scrape/bing.js
+++ scrape/bing.js
@@ -4,13 +4,13 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 /**
- * Searches DuckDuckGo given some query
+ * Searches Bing given some query
  * @param {String} query - search query
  */
 
 module.exports = {
     searchBing: function (searchQuery) {
-        return request("https://www.bing.com/search?q" + searchQuery, (error, response, html) => {
+        return request("https://www.bing.com/search?q=" + searchQuery, (error, response, html) => {
             if (error) {
                 console.log(error);
             } else if (response.statusCode !== 200) {
@@ -19,7 +19,13 @@ module.exports = {
                 // load cheerio
                 const $ = cheerio.load(html);
                 const searchResults = $("#b_results");
-                console.log(searchResults, searchResults.length);
+                const result = searchResults.map((i, el) => {
+                    console.log("mapping");
+                    return {
+                        title: $(el).find("a").text()
+                    };
+                });
+                console.log("final result is ", result);
             }
         });
     }
