c40c0f34ee2744f5962f6429e10983ff1def71fb
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-28 23:17:36 +0100

    Change let to const and declare results array

--- scrape/google.js
+++ scrape/google.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let google = require('google');
-let Lists = require('./lists');
+const google = require('google');
+const Lists = require('./lists');
 google.resultsPerPage = 25;
 
 /**
@@ -10,20 +10,21 @@ google.resultsPerPage = 25;
  * @param { Function } callback - The standard node callback
  */
 module.exports = {
-    searchGoogle: function (query, callback) {
+    search: function (query, callback) {
         let nextCounter = 0;
+        const results = [];
         google(query, (err, next, links) => {
             if (err) {
                 callback(err);
             } else {
-                let newLinks = links.map(link => {
+                const newLinks = links.map(link => {
                     return {
                         title: link.title,
                         link: link.link,
                         description: link.description
                     };
                 });
-                Lists.appendAll(results, newLinks);
+                results = Lists.appendAll(results, newLinks);
                 if (nextCounter < 4) {
                     nextCounter += 1;
                     if (next) {
