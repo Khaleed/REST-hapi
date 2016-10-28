7fbaefbe3ec3bf062dbf46bc52978f8cdc9bbfb9
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-28 23:24:38 +0100

    Set up the express server using async to handle the three asynchronous callbacks

--- server/app.js
+++ server/app.js
@@ -1,58 +1,23 @@
 "use strict";
 
-let cheerio = require('cheerio');
-let express = require('express');
-let app = express();
+const google = require('../scrape/google.js');
+const yahoo = require('../scrape/yahoo.js');
+const bing = require('../scrape/bing.js');
+const express = require('express');
+const async = require('async');
+const app = express();
 
-// RESTful API that gets aggregated results
 app.get('/aggregate_search', (req, res) => {
-    // 3 async requests
-
-    var search_query = req.params.query;
-
-    // flag => false
-    var x = [false, false];
-
-    search_google(search_query, (err, result)  => {
-        x[0] = true;
-        //do something with google response
-        if (x.every(x => x == true)) {
-            res.send();
-        }
+    const query = req.params.query;
+    async.parallel([
+        callback => google.search(query, callback),
+        callback => yahoo.search(query, callback),
+        callback => bing.search(query, callback)
+    ], (err, results) => {
+        res.end(JSON.stringify(results));
     });
-    search_duck(search_query, (err, result) => {
-        x[1] = true;
-        //do something with duck duck go response
-        if (x.every(x => x == true)) {
-            res.send()
-        }
-    });
-
-    //Promise.all
-
-    //when can you res.send()?
-
-    /*getResponse((err, result) => {
-      if (err) {
-      console.log(err);
-      res.status(400).send('Something is broken');
-      } else {
-      res.end(JSON.stringify(result));
-      }
-      });*/
 });
 
-let getResponse = callback => {
-    var req_callback = function (error, response, html) {
-        if (error) {
-            console.log('error');
-            callback(error);
-        } else if (response.statusCode != 200) {
-            console.log(response.statusCode);
-            callback(new Error('error try again'));
-        } else {
-            // parse data
-        };
-        callback(null, metadata);
-    };
-};
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
